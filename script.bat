50b3cc36fe413c7d86856458be76f9f5f44aae45
Carol <redrumjedi@gmail.com>
2021-06-17 20:16:12 -0300

    removed .bin extension

--- script.bat
+++ script.bat
@@ -3,7 +3,7 @@ python mml2mfvi.py %*
 Setlocal EnableDelayedExpansion
 @for /f "delims=" %%i in ('dir /b *.bin')  do (
     set fname=%%~ni
-    set fname=!fname:~0,-5!.bin
+    set fname=!fname:~0,-5!
     ren "%%i" "!fname!"
 )
 endlocal
